c185d143a59be02f1f3576ed04cc11c5494e1d4c
A.Prokopenko <70268975+OneEyed1366@users.noreply.github.com>
2022-03-10 10:27:51 +0300

    Update useTypedSelector.ts

--- src/hooks/useTypedSelector.ts
+++ src/hooks/useTypedSelector.ts
@@ -1,6 +1,4 @@
 import { RootState } from '../store';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
-export default function useTypedSelector(): TypedUseSelectorHook<RootState> {
-  return useSelector;
-}
+export default useSelector as TypedUseSelectorHook<RootState>;
