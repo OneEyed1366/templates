e182f34f74ba8e3a55bb838194a4f2c73e56f73d
psevdo:Proger <psevdoproger@yandex.ru>
2022-04-02 23:42:14 +0300

    init

--- core/settings/internalizaion.py
+++ core/settings/internalizaion.py
@@ -1,10 +1,18 @@
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
+from django.utils.translation import gettext_lazy as _
 
-LANGUAGE_CODE = 'en-us'
+from core.settings import BASE_DIR
 
+LOCALE_PATHS = [
+    BASE_DIR / "i18n",
+]
+LANGUAGE_CODE = 'ru'
+LANGUAGES = (
+    ("en", _("English")),
+    ("ru", _("Russian"))
+)
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
