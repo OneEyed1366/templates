365f997abf799302a94f8bba0f4f53f27a0dd614
A.Prokopenko <70268975+OneEyed1366@users.noreply.github.com>
2022-07-09 11:37:19 +0300

    Create "rewrite_apps" method

--- apps/custom_commands__app/management/commands/create.py
+++ apps/custom_commands__app/management/commands/create.py
@@ -6,6 +6,19 @@ from os import makedirs
 
 
 class Command(BaseCommand):
+    def rewrite_apps(self, app_name, file_destination):
+        origin: str
+        new_content: str
+
+        with open(file_destination, "r", encoding="UTF-8") as apps_file:
+            origin = apps_file.read()
+            new_content = origin.replace(app_name, f"apps.{app_name}")
+
+        with open(file_destination, "w+", encoding="UTF-8") as new_apps_file:
+            new_apps_file.write(new_content)
+
+
+
     def add_arguments(self, parser):
         parser.add_argument(
             nargs="+",
@@ -14,9 +27,12 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        destination = f"{args[0]}__app"
-        full_destination = Path(f"apps/{destination}")
+        new_app_name = f"{args[0]}__app"
+        full_destination = Path(f"apps/{new_app_name}")
+        apps_destination = Path(f"apps/{new_app_name}/apps.py")
         path = BASE_DIR / full_destination
 
         makedirs(path)
-        call_command("startapp", destination, full_destination)
+        call_command("startapp", new_app_name, full_destination)
+
+        self.rewrite_apps(new_app_name, apps_destination)
